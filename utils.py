1627beb487a124387e8aae6e911911914dbc510b
Nicole Gu <nicoleguob@gmail.com>
2024-07-13 16:19:56 -0700

    Not enough space on GPU need to move to collab probably...?

--- utils.py
+++ utils.py
@@ -12,6 +12,15 @@ def setup_rich():
     return console
 
 
-def load_envs(envs=".env/"):
+def load_envs(envs=".env/", **kwargs):
     for env in os.listdir(envs):
         load_dotenv(dotenv_path=os.path.join(envs, env))
+
+
+def setup(**kwargs):
+    load_envs(**kwargs)
+    # project_id = os.getenv("PROJECT_ID")
+    # session_id = os.getenv("SESSION_ID")
+    # collection_name = os.getenv("COLLECTION_NAME")
+    console = setup_rich()
+    return console
